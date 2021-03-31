09bf682f443cd0a746404897cd8df691cf3e3673
anisanurfadila <anisanfd925@gmail.com>
2021-03-31 13:39:33 +0700

    pendaftaran

--- app/src/main/java/com/example/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.constraintlayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()== R.id.mnDaftar)
+        {
+            Intent i = new Intent(getApplicationContext(), Pendaftaran_activity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     Button btnLogin;
     EditText emailEd, passwordEd;
